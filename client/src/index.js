63fc8c6608f12d769e590cbb62dbd96c955aaa59
Ahmet Özdemir <69878794+AhmetOzdemir34@users.noreply.github.com>
2022-06-22 10:00:07 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,11 @@ import 'tailwindcss/tailwind.css';
 import 'antd/dist/antd.min.css';
 import './global.css';
 import App from './App';
-import {store} from './redux/store'
-import {Provider} from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
+    
         <App />
-    </Provider>
+    
 );
 
